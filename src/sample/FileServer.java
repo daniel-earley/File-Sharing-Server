05aae862068aae9b08d64dec0c8cc11f0518a5aa
daniel-earley <danielse08@gmail.com>
2021-04-01 21:20:17 -0400

    removed commented code

--- src/sample/FileServer.java
+++ src/sample/FileServer.java
@@ -1,13 +1,12 @@
 package sample;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class FileServer {
-    private ServerSocket serverSocket = null;
-    private static File sharedFolder = null;
+    private static ServerSocket serverSocket = null;
     private int port;
 
     public FileServer(){}
@@ -32,31 +31,14 @@ public class FileServer {
     public static void main(String[] args){
         int port = 8080;
 
-//        if (args.length > 0){
-//            // first arg is computer name
-//            // 2nd arg is shared folder (server folder)
-//            System.out.println("Welcome " + args[0]);
-//            if (args.length > 1) {
-//                System.out.printf("File path is -> %s\n", args[1]);
-//                setSharedFolder(new File(args[1]));
-//            }
-//        }
-
         try {
             FileServer server = new FileServer(port);
             server.handleRequests();
         } catch (IOException e){
             e.printStackTrace();
         }
-
     }
 
-    public static File getSharedFolder() {
-        return sharedFolder;
-    }
 
-    public static void setSharedFolder(File folder) {
-        sharedFolder = folder;
-    }
 
 }
